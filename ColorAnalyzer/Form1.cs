9596081792d4814524a81fd296287b8a19ee98aa
NattyXO <nattyxo2018@gmail.com>
2023-12-26 12:59:47 +0300

    Image Selection,Error Handling,add RAEDME.md

--- ColorAnalyzer/Form1.cs
+++ ColorAnalyzer/Form1.cs
@@ -11,6 +11,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
 
 namespace ColorAnalyzer
 {
@@ -24,57 +26,52 @@ namespace ColorAnalyzer
         private int CountColors()
         {
             int result = 0;
-            
             List<Color> colors = new List<Color>();
             Color pixelColor;
 
-            Bitmap picture = new Bitmap("test0.png");
+         
+                Bitmap picture = new Bitmap(pic_preview.Image);
 
-
-
-
-
-            BitmapData bitmapData = picture.LockBits(new Rectangle(0, 0, picture.Width, picture.Height), ImageLockMode.ReadWrite, picture.PixelFormat);
-            int BytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(picture.PixelFormat) / 8;
-            int ByteCount = bitmapData.Stride * picture.Height;
-            byte[] Pixels = new byte[ByteCount];
-            IntPtr PtrFirstPixel = bitmapData.Scan0;
-            Marshal.Copy(PtrFirstPixel, Pixels, 0, Pixels.Length);
-            int HeightInPixels = bitmapData.Height;
-            int WidthInBytes = bitmapData.Width * BytesPerPixel;
-            for (int y = 0; y < HeightInPixels; y++)
-            {
-                int CurrentLine = y * bitmapData.Stride;
-                for (int x = 0; x < WidthInBytes; x = x + BytesPerPixel)
+                BitmapData bitmapData = picture.LockBits(new Rectangle(0, 0, picture.Width, picture.Height), ImageLockMode.ReadWrite, picture.PixelFormat);
+                int BytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(picture.PixelFormat) / 8;
+                int ByteCount = bitmapData.Stride * picture.Height;
+                byte[] Pixels = new byte[ByteCount];
+                IntPtr PtrFirstPixel = bitmapData.Scan0;
+                Marshal.Copy(PtrFirstPixel, Pixels, 0, Pixels.Length);
+                int HeightInPixels = bitmapData.Height;
+                int WidthInBytes = bitmapData.Width * BytesPerPixel;
+                for (int y = 0; y < HeightInPixels; y++)
                 {
-                    int OldBlue = Pixels[CurrentLine + x];
-                    int OldGreen = Pixels[CurrentLine + x + 1];
-                    int OldRed = Pixels[CurrentLine + x + 2];
-                    pixelColor = Color.FromArgb(255, OldRed, OldGreen, OldBlue);
-
-
-                    if (colors.Contains(pixelColor))
+                    int CurrentLine = y * bitmapData.Stride;
+                    for (int x = 0; x < WidthInBytes; x = x + BytesPerPixel)
                     {
-                        
-                    }
-                    else
-                    {
-                        colors.Add(pixelColor);
-                    }
+                        int OldBlue = Pixels[CurrentLine + x];
+                        int OldGreen = Pixels[CurrentLine + x + 1];
+                        int OldRed = Pixels[CurrentLine + x + 2];
+                        pixelColor = Color.FromArgb(255, OldRed, OldGreen, OldBlue);
 
 
-                    pixelColor = Color.Empty;
+                        if (colors.Contains(pixelColor))
+                        {
+
+                        }
+                        else
+                        {
+                            colors.Add(pixelColor);
+                        }
+
+
+                        pixelColor = Color.Empty;
+
+                    }
 
                 }
 
-            }
-         
-            result = colors.Count;
-            picture.UnlockBits(bitmapData);
-           // MessageBox.Show(string.Format("Colors: {0}", result.ToString()));
+                picture.UnlockBits(bitmapData);
+                result = colors.Count;
+
             return result;
-
-
+            // MessageBox.Show(string.Format("Colors: {0}", result.ToString()));
 
 
             //int bytesPerPixel = 3;
@@ -121,32 +118,72 @@ namespace ColorAnalyzer
             //    }
             //}
 
-
-
         }
 
         private void btn_CountColors_Click(object sender, EventArgs e)
         {
-            int result = 0;
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
+            if (pic_preview.Image != null)
+            {
+                int result = 0;
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
 
-            result = CountColors();
-            stopWatch.Stop();
-            // Get the elapsed time as a TimeSpan value.
-
-
-            // Format and display the TimeSpan value.
-            //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-            //    ts.Hours, ts.Minutes, ts.Seconds,
-            //    ts.Milliseconds / 10);
+                result = CountColors();
+                stopWatch.Stop();
+                // Get the elapsed time as a TimeSpan value.
 
 
-            //string path = @"psf\Home\Desktop\мельник.jpeg";
+                // Format and display the TimeSpan value.
+                //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                //    ts.Hours, ts.Minutes, ts.Seconds,
+                //    ts.Milliseconds / 10);
 
-            MessageBox.Show(string.Format("Colors:{0} , Runtime: {1}", result, stopWatch.Elapsed));
 
+                //string path = @"psf\Home\Desktop\мельник.jpeg";
 
+                MessageBox.Show(string.Format("Colors: {0}, Runtime: {1}", result, stopWatch.Elapsed));
+            }
+            else
+            {
+                MessageBox.Show("Please select an image first.", "No Image Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files (*.png; *.jpg; *.jpeg; *.gif; *.bmp)|*.png; *.jpg; *.jpeg; *.gif; *.bmp|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string selectedFileName = openFileDialog1.FileName;
+
+                    // Display the selected image in the PictureBox
+                    using (Bitmap selectedImage = new Bitmap(selectedFileName))
+                    {
+                        pic_preview.Image = new Bitmap(selectedImage); // Make a copy to avoid issues with file locks
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("The selected file is not a valid image file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void btn_github_Click(object sender, EventArgs e)
+        {
+            string githubUrl = "https://github.com/whoiskatrin/color-analyzer-winform";
+
+            // Open the default browser with the specified URL
+            Process.Start(githubUrl);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,44 @@
+# Color Analyzer WinForm
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/whoiskatrin/color-analyzer-winform/blob/main/LICENSE)
+
+Color Analyzer WinForm is a simple Windows application built in C# using WinForms that analyzes the colors present in an image.
+
+## Features
+
+- **Color Counting**: Calculates the number of unique colors in an image.
+- **Image Selection**: Allows users to load an image in various formats (PNG, JPG, JPEG, GIF, BMP) for analysis.
+- **User-Friendly Interface**: A straightforward interface enabling users to perform color analysis with ease.
+
+## Installation
+
+Clone the repository to your local machine using:
+
+```bash
+git clone https://github.com/whoiskatrin/color-analyzer-winform.git
+```
+
+## Usage
+
+1. Run the application.
+2. Click on the "Browse" button to select an image.
+3. Click on "Count Colors" to perform the color analysis.
+4. View the total number of colors present in the loaded image.
+
+## Screenshots
+<br>
+<div align="center">
+    <img src="https://github.com/whoiskatrin/sql-translator/blob/main/UI1.png" width="600" />
+</div>
+<br>
+<div align="center">
+    <img src="https://github.com/whoiskatrin/sql-translator/blob/main/UI2.png" width="600" />
+</div>
+
+## Contributing
+
+Contributions are welcome! Please fork the repository and create a pull request for any new features, improvements, or bug fixes.
+
+## License
+
+This project is licensed under the [MIT License](https://github.com/whoiskatrin/color-analyzer-winform/blob/main/LICENSE).
